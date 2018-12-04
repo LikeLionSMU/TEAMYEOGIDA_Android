0e3aa2d83a1631850a05f70833294f704e2920c9
DESKTOP-S9BD7ED\nov-j <Lion.novjoon@gmail.com>
2018-12-04 19:00:52 +0900

    1204_디자인

--- yeogida8/app/src/main/java/com/example/hahaj/yeogida8/BuyItemActivity.java
+++ yeogida8/app/src/main/java/com/example/hahaj/yeogida8/BuyItemActivity.java
@@ -24,7 +24,7 @@ public class BuyItemActivity extends AppCompatActivity {
     //지도API
     //LinearLayout mapview;
     TMapView tmap;
-    private final String MAPAPPKEY = "";
+    private final String MAPAPPKEY = "1947620f-e0aa-43a5-8e27-11c6d1dab9b5";
     double lat;
     double lng;
 
